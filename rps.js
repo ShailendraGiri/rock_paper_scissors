50d7ce3243d1023b0abd5b0bf98fb9f76220fc27
ShailendraGiri <122157894+ShailendraGiri@users.noreply.github.com>
2023-03-11 11:39:19 +0545

    using random number and array

---
+++ rps.js
@@ -0,0 +1,10 @@
+//random selection out of an array//
+const options=["rock","paper","scissors"]
+//generating a random number between 0 and 1//
+let x=Math.floor(Math.random()*10);
+if (x<3){
+    console.log(options[x]);
+}
+
+
+
