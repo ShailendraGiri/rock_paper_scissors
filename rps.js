dcab3cae5e91bc8e39ba4da476572366b59f4895
ShailendraGiri <122157894+ShailendraGiri@users.noreply.github.com>
2023-03-12 08:11:56 +0545

    completed using if..else if statements

--- rps.js
+++ rps.js
@@ -1,10 +1,20 @@
-//random selection out of an array//
-const options=["rock","paper","scissors"]
-//generating a random number between 0 and 1//
-let x=Math.floor(Math.random()*10);
-if (x<3){
-    console.log(options[x]);
+//for human choice//
+let humanChoice=prompt("please enter a choice: ");
+console.log("You chose "+humanChoice);
+//for computer choice//
+const rps=["rock","paper","scissors"];
+let x=Math.floor(Math.random()*3);
+let computerChoice=rps[x];
+console.log("The computer chose "+computerChoice);
+//conditions for draw, win and loss
+if(humanChoice==computerChoice){
+    console.log("It's a draw");
+}else if(humanChoice=="rock" && computerChoice =="paper"|| humanChoice=="paper"&& computerChoice=="scissors"||humanChoice=="scissors" && computerChoice=="rock"){
+    console.log("The computer won");
+}else if (humanChoice=="rock"&& computerChoice=="scissors"||humanChoice=="paper" && computerChoice=="rock"||humanChoice=="scissors" && computerChoice=="paper"){
+    console.log("You won");
 }
+
 
 
 
