cde33940c57cd5fae1361f5338cf5b02f6267a1b
ShailendraGiri <122157894+ShailendraGiri@users.noreply.github.com>
2023-03-15 21:23:50 +0545

    finalisation of the project, GUI will be added next.

--- rps.js
+++ rps.js
@@ -1,3 +1,8 @@
+let newHumanScore=0;
+let newComputerScore=0;
+console.log("Welcome to the rock, paper, scissors game.")
+console.log("The game will be played for 5 rounds and the winner will be decided.")
+console.log("Please enter a choice between rock, paper and scissors.")
 //for human choice//
 function getHumanChoice(){
     let humanChoice=prompt("please enter a choice: ");
@@ -21,17 +26,32 @@ function getComputerChoice(){
 function playRound(humanChoice,computerChoice){
     if(humanChoice==computerChoice){
         console.log("It's a draw");
+        return{humanScore:0,computerScore:0};
     }else if(humanChoice=="rock" && computerChoice =="paper"|| humanChoice=="paper"&& computerChoice=="scissors"||humanChoice=="scissors" && computerChoice=="rock"){
         console.log("The computer won");
+        return{humanScore:0,computerScore:1};
     }else if (humanChoice=="rock"&& computerChoice=="scissors"||humanChoice=="paper" && computerChoice=="rock"||humanChoice=="scissors" && computerChoice=="paper"){
         console.log("You won");
+        return{humanScore:1,computerScore:0}
     }
 }
 
     for(let x=1;x<=5;x++){//for 5 rounds of the game//
         let humanChoice=getHumanChoice();
         let computerChoice=getComputerChoice();
-        playRound(humanChoice,computerChoice);
+        let {humanScore,computerScore}=playRound(humanChoice,computerChoice);//destructuring the object//
+        newHumanScore+=humanScore;
+        newComputerScore+=computerScore;
+    }
+    console.log("Here is the result of the game.");
+    console.log("Your total score is "+newHumanScore);
+    console.log("The computer's total score is "+newComputerScore);
+    if(newHumanScore==newComputerScore){
+        console.log("It's a draw");
+    }else if (newHumanScore>newComputerScore){
+        console.log("You won");
+    }else if (newHumanScore<newComputerScore){
+        console.log("The computer won");
     }
 
 
